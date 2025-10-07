ed70fc0085740699d913d15790ed9de04bdf88f3
KENE. <ubesiekene@gmail.com>
2025-10-07 20:59:21 +0100

    update to tailwind cli

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
 AOS.init();
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
- speed: 400,
- spaceBetween: 10,
- slidesPerView: 6,
+  speed: 400,
+  spaceBetween: 10,
+  slidesPerView: 6,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
+
+  // Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 6,
+    },
+  },
+
+  loop: true,
+
+  // autoplay: {
+  //   delay: 2500,
+  //   disableOnInteraction: false,
+  // },
 
   // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: ".swiper-scrollbar",
   },
 });
 
-
-const menuBtn = document.getElementById('menuBtn');
-const sidenav = document.getElementById('sidenav');
-menuBtn.addEventListener('click',() => {
-    sidenav.classList.toggle('hidden');
- });
+const menuBtn = document.getElementById("menuBtn");
+const sidenav = document.getElementById("sidenav");
+menuBtn.addEventListener("click", () => {
+  sidenav.classList.toggle("hidden");
+});
 
 // const navbar = document.getElementById('navbar');
 // const scrolledClass = 'bg-black/5 backdrop-blur-none';
@@ -42,4 +61,4 @@ menuBtn.addEventListener('click',() => {
 // } else {
 //     navbar.ClassList.remove(...scrolledClass.split(''));
 //     navbar.ClassList.add(...initialClass.split(''));
-// };  
+// };
