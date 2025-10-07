5028cf87c12963e9b84c6c1d15717617c8b29b37
Ella <ellaoyins@gmail.com>
2025-10-07 16:55:57 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+AOS.init();
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+ speed: 400,
+ spaceBetween: 10,
+ slidesPerView: 6,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+
+const menuBtn = document.getElementById('menuBtn');
+const sidenav = document.getElementById('sidenav');
+menuBtn.addEventListener('click',() => {
+    sidenav.classList.toggle('hidden');
+ });
+
+// const navbar = document.getElementById('navbar');
+// const scrolledClass = 'bg-black/5 backdrop-blur-none';
+// const initialClass = 'bg-black/5 backdrop-blur-sm';
+
+// const scrollHandler = () => {
+//     if (window.scrollY > 50){
+//         navbar.classList.add(scrolledClass);
+//         navbar.ClassList.remove(initialClass);
+//     }
+// } else {
+//     navbar.ClassList.remove(...scrolledClass.split(''));
+//     navbar.ClassList.add(...initialClass.split(''));
+// };  
